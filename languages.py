83febe902abb19ba871cb3c3cc2993fda9dc9e7e
ROSTIG-NOX3N <137249382+ROSTIG-NOX3N@users.noreply.github.com>
2024-09-13 11:07:06 +0900

    Update languages.py

--- languages.py
+++ languages.py
@@ -23,9 +23,9 @@ def get_labels(language):
             'half_life': 'Half-life (years)',  # 영어로 통일
             'isotope_index': 'Isotope Index',  # 영어로 통일
             'closest': '지질의 연대와 가장 가까운 동위원소',
-            'selected': '선택된 동위원소:',
+            'selected': 'Selected Isotope:',
             'input_age_label': '입력된 연대:',
-            'annotate_closest': '입력된 연대에 가장 가까운 동위원소',
-            'annotate_selected': '선택된 동위원소:',
+            'annotate_closest': 'Closest to',
+            'annotate_selected': 'Selected Isotope:',
             'half_life_of_selected_isotope': '선택된 동위원소의 반감기(년)'
         }
